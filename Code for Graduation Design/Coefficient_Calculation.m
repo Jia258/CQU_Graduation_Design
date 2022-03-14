4a823244c7e057cf48db7c0e2820d02da4172eff
Jia258 <1211278977@qq.com>
2022-03-14 22:39:40 +0800

    03.14

---
+++ Code for Graduation Design/Coefficient_Calculation.m
@@ -0,0 +1,4 @@
+function [Coefficient] = Coefficient_Calculation()
+    %% 基本常数定义
+    Hdr_PDCP = 2; Hdr_RLC = 5; Hdr_MAC = 2;
+end
---
+++ Code for Graduation Design/Global Constant.m
@@ -0,0 +1,13 @@
+%% 全局常量定义
+
+%% 公共常量定义
+global A_ref = 2, B_ref = 10, M_ref = 6, C_ref = 5/6, LMIMO_ref = 2, Ld_ref = 1
+
+%% 计算资源模型常量定义
+global SIGMA1 = 0.2, SIGMA2 = 0.3, SIGMA3 = 0.5
+global G1_ref = 200, G2_ref = 20, G3_ref = 10, G4_ref = 30, G5_ref = 80, G6_ref = 720
+
+%% 带宽资源模型常量定义
+global n_RB = 100, fs = 30.72
+global alpha_0, alpha_1, alpha_2, alpha_3, alpha_4, alpha_5, alpha_6, alpha_7, alpha_8
+global beta_1, beta_2
